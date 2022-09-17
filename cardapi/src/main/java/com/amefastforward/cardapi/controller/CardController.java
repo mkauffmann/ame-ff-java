a367911bc476ac7f630cb0fb218bbff72ce0f6f6
Maria Kauffmann <70024598+mkauffmann@users.noreply.github.com>
2022-09-17 16:20:55 -0300

    estrutura basica dados mockados

---
+++ cardapi/src/main/java/com/amefastforward/cardapi/controller/CardController.java
@@ -0,0 +1,33 @@
+package com.amefastforward.cardapi.controller;
+
+import com.amefastforward.cardapi.model.Card;
+import com.amefastforward.cardapi.service.CardService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/card")
+public class CardController {
+    private final CardService cardService;
+    private static final Logger LOG = LoggerFactory.getLogger(CardController.class);
+
+    @Autowired
+    public CardController(CardService cardService) {
+        this.cardService = cardService;
+    }
+
+    @GetMapping("/{id}")
+    public Card findCardById(@PathVariable("id") long id){
+        var card = cardService.findById(id);
+
+        if(card.isPresent()){
+            return card.get();
+        }
+    }
+}
---
+++ cardapi/src/main/java/com/amefastforward/cardapi/repository/impl/CardRepositoryImpl.java
@@ -0,0 +1,54 @@
+package com.amefastforward.cardapi.repository.impl;
+
+import com.amefastforward.cardapi.model.Card;
+import com.amefastforward.cardapi.repository.CardRepository;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class CardRepositoryImpl implements CardRepository {
+    private final List<Card> cards;
+
+    public CardRepositoryImpl() {
+        this.cards = new ArrayList<Card>();
+
+        var card = new Card();
+        card.setId(1);
+        card.setName("Iron Man");
+        card.setImageUrl("imgUrl");
+        card.setStrength(5);
+        card.setSpeed(5);
+        card.setGear(5);
+        card.setIntellect(8);
+        card.setSkill(6);
+        card.setCreatedAt(LocalDateTime.now());
+        card.setUpdateAt(LocalDateTime.now());
+
+        cards.add(card);
+
+        card = new Card();
+        card.setId(2);
+        card.setName("Spider Man");
+        card.setImageUrl("imgUrl");
+        card.setStrength(5);
+        card.setSpeed(7);
+        card.setGear(4);
+        card.setIntellect(7);
+        card.setSkill(6);
+        card.setCreatedAt(LocalDateTime.now());
+        card.setUpdateAt(LocalDateTime.now());
+
+        cards.add(card);
+
+
+    }
+
+    @Override
+    public Optional<Card> findById(long id) {
+        return cards.stream()
+                .filter(card -> card.getId() == id)
+                .findFirst();
+    }
+}
